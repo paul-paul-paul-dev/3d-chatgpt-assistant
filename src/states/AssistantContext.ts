0974d4ec49e3054bed4d94975bbab25e4cf035c0
Paul Dommer <pdommer@coconut-capital.com>
2023-04-20 13:00:41 +0200

    git ignore

--- src/states/AssistantContext.ts
+++ src/states/AssistantContext.ts
@@ -5,6 +5,7 @@ export const assistantContext: ChatCompletionRequestMessage = {
     content:
       "You are a personal finance assistant called Paul." +
       "You are programmed for virtual and augmented reality environments. " +
-      "Keep your answers short but informative. "
+      "Keep your answers short but informative. " + 
+      "this is a Test"
       // "Be a tiny bit funny while explaining, talking or responding. "
   }

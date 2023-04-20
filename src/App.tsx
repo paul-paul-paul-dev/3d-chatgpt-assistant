8c03470c00535ab6b60e2b63a625885fd3fc06c0
Paul Dommer <pdommer@coconut-capital.com>
2023-04-20 14:26:17 +0200

    Remove TextInput completely for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import Assitant from "./components/Assistant";
-import TextInput from "./components/TextInput";
 import { inputCanvasProps } from "@coconut-xr/input";
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
         <ambientLight intensity={0.5} />
         <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
         <pointLight position={[-10, -10, -10]} />
-        {/* <TextInput /> */}
         <Assitant position={[0, 1, 0]} />
         <OrbitControls enableRotate={false} />
       </Canvas>
